ff52dbb5489e76d68b312a4cc19cac3d56448ab9
Manoj Patil M <smmanoj208@gmail.com>
2022-11-28 17:32:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,14 @@
 from version import get_version
 import os
 import flask
-app = Flask(__name__)
-PASS=os.environ['PASS']
 
 
-@app.route('/')
+
 def version():
+   PASS=os.environ.get('PASSWORD')
 
    print(PASS)
     
 if __name__ == '__main__':
-    app.run(port=80,debug=True)
+    version()
 
