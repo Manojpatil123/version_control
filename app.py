b0c7b78cbf2891d266ddc9f69e6f71cb577cd6c3
Manoj Patil M <smmanoj208@gmail.com>
2022-11-28 21:28:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,10 @@ def report1():
               
          imei_no=str(dataframe.iloc[i,0])#getting imei from sheet        
          df=sensortracking.sql_connection(imei_no,DB_PASSWORD)
-         if(len(df)>0);
+         if(len(df)>0):
            print('connection')
+         else:
+             print('no connection'
          df1=pd.DataFrame(df,columns=['id','timestamp','imei_no','temperature','pressure','humidity','rainfall','windspeed','winddirection','soil_moisture1','soil_moisture2','lw','soil_temperature','lux','flow_meter','raw_data','created_at','updated_at'])
          
          if(df1.shape[0]==0):#checking values are there or not in dataframe      
