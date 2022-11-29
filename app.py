55b1f9e01230ce9babafea17b595c08b9c1620dd
Manoj Patil M <smmanoj208@gmail.com>
2022-11-29 08:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 
 
 #getting all secrets
-MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
 DB_PASSWORD=os.environ.get('DB_PASSWORD')
 JIRA_API=os.environ.get('JIRA_TOCKEN')
 
